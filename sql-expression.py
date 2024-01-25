47ca4926b832ecb5d9b7bdcbe9eef2eb7644a1c1
Jessica Clark <jesclark28@gmail.com>
2024-01-25 07:30:03 +0000

    Final commit morning

--- sql-expression.py
+++ sql-expression.py
@@ -4,3 +4,10 @@ from sqlalchemy import (
 
 # Link python file to Chinook database using create_engine component. 
 db = create_engine("postgresql:///chinook") # create variable db and tell it to point to postgresql db called chinook. 
+# three slashes means database is hosted locally within workspace environment. 
+
+meta = MetaData(db) # Use the MetaData class and save to variabe meta. Data about and within tables.
+
+# connect to the database, using the .connect() method, and the Python with-statement.
+# saves connection to database in the connection variable. 
+with db.connect() as connection:  

9e0d9348b5c394df072479495aade00064e74ca2
Jessica Clark <jesclark28@gmail.com>
2024-02-06 08:24:39 +0000

    Create session class to call session to access db

--- sql-orm.py
+++ sql-orm.py
@@ -6,3 +6,13 @@ from sqlalchemy import (
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook") # Using postgres server on a local host (three slashes = localhost)
+base = declarative_base() # variable called base - grabs metadata from database then hands it back in a sub-class. 
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined above
+session = Session()

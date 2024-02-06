3e7561a9d493cbf8845ed3a5bf11137050305fab
Jessica Clark <jesclark28@gmail.com>
2024-02-06 08:34:28 +0000

    Add class based models

--- sql-orm.py
+++ sql-orm.py
@@ -11,6 +11,39 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook") # Using postgres server on a local host (three slashes = localhost)
 base = declarative_base() # variable called base - grabs metadata from database then hands it back in a sub-class. 
 
+
+# Add class based models before the session is created 
+# but after the class is declared
+
+# create a class-based model for the "Artist" table
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+
+
+# create a class-based model for the "Album" table
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
+
+# create a class-based model for the "Track" table
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True)
+    Name = Column(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(Integer, primary_key=False)
+    Bytes = Column(Integer, primary_key=False)
+    UnitPrice = Column(Float)
+
+
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)

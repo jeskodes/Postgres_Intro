ace5f9f284f6ac426b146cdc50e01814aa2f61bb
Jessica Clark <jesclark28@gmail.com>
2024-02-07 17:08:49 +0000

    Add record to table and commit

--- sql-crud.py
+++ sql-crud.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook") # Using postgres server on a local host (three slashes = localhost)
 base = declarative_base() # variable called base - grabs metadata from database then hands it back in a sub-class. 
 
-# Create a class-based model for the "Programmer" table
+# Create a class-based model for the "Programmer" table.  This is the table schema. 
 class Programmer(base): 
     __tablename__ = "Programmer"
     id = Column(Integer, primary_key=True) #The primary key is id and it will auto increment as add more to it
@@ -32,3 +32,19 @@ session = Session()
 
 # creating the database using declarative_base subclass
 base.metadata.create_all(db) # using creeate_all() method. 
+
+# Create records on table. 
+# Record #1 
+ada_lovelace = Programmer (
+    first_name = "Ada",
+    last_name = "Lovelace",
+    gender = "Female",
+    nationality = "British",
+    famous_for = "First Programmer"
+)
+
+# Add each programmer to the table
+session.add(ada_lovelace)
+
+# Commit to the table
+session.commit()
